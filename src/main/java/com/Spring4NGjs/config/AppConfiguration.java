c5214c2f87b54b322f4ea87999b7bfc7cf001248
peanutive00 <150366111@stu.vtc.edu.hk>
2018-08-04 16:54:51 +0800

    merge WebConfig to AppConfig

--- src/main/java/com/Spring4NGjs/config/AppConfiguration.java
+++ src/main/java/com/Spring4NGjs/config/AppConfiguration.java
@@ -3,10 +3,17 @@ package com.Spring4NGjs.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.Spring4NGjs")
-public class AppConfiguration {
+public class AppConfiguration extends WebMvcConfigurerAdapter {
+    
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/app/**").addResourceLocations("/app/");
+    }
     
 }
